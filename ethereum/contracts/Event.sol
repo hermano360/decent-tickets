b76c1396e7ac68c39cb95ef6d09eb9d97d7644cd
Herminio Garcia <herminio@budsy.io>
2018-08-01 00:35:46 -0700

    1.104 Contributing to the Campaign

--- ethereum/contracts/Event.sol
+++ ethereum/contracts/Event.sol
@@ -3,10 +3,15 @@ pragma solidity ^0.4.17;
 contract Event {
   address public manager;
   uint public minimumContribution;
+  address[] public approvers;
 
-
-  function Campaign(uint minimum) public {
+  constructor (uint minimum) public {
     manager = msg.sender;
     minimumContribution = minimum;
+  }
+
+  function contribute() public payable {
+      require(msg.value > minimumContribution);
+      approvers.push(msg.sender);
   }
 }
